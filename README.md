40f3da7729779b00a18626ad4a1ed814e4813ee9
egy186 <egy_family_384-github@yahoo.co.jp>
2013-08-30 14:25:48 +0900

    v0.1.0a

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 RGB、HSLなどCSSで用いられる色の表記を相互に変換するためのWebサイト。
 
+この色ってRGB（HSL）だったらどうなるの？とか、  
+どの色にしようかな？とか思ったときに使うサイトです。（たぶん^^;
+
 ## Alpha版ですよ
 
 細かいとこは気にしない、  
 JavaScriptも実行速度無視、  
-RGBからの変換にしか対応してない。
+<del>RGBからの変換にしか対応してない。</del>  
+Version 0.1.0 で対応済。
 
 ## 動作確認済みのブラウザ
 
 * Internet Explorer 10
-* Mozilla Firefox 23 beta
+* Mozilla Firefox 23
 * Google Chrome 28
 
 要するに&lt;input type="range" /&gt;が実装されているモダンブラウザなら動くはずです。
--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-﻿(function () {
-    queryParam = {};
-    queryParam.get = function (thisParam) {
-        if (!location.search) {
-            return null;
-        }
-        var queryObj = {},
-            tmp,
-            query = location.search.substring(1).split(/&/g);
-        for (var i = 0; i < query.length; i++) {
-            tmp = query[i].split('=');
-            queryObj[decodeURIComponent(tmp[0])] = decodeURIComponent(tmp[1]);
-        }
-        if (thisParam) {
-            return queryObj[thisParam]
-        }
-        return queryObj;
+﻿queryParam = {};
+queryParam.get = function (thisParam) {
+    if (!location.search) {
+        return null;
     }
-    queryParam.set = function (paramObj) { }
-})();
+    var queryObj = {},
+        tmp,
+        query = location.search.substring(1).split(/&/g);
+    for (var i = 0; i < query.length; i++) {
+        tmp = query[i].split('=');
+        queryObj[decodeURIComponent(tmp[0])] = decodeURIComponent(tmp[1]);
+    }
+    if (thisParam) {
+        return queryObj[thisParam]
+    }
+    return queryObj;
+}
+queryParam.set = function (paramObj) { }
+
+
