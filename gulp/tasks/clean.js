1ecb72fb465ac7627f795826f1fc4aea3c467eff
egy186 <egy186@yahoo.co.jp>
2015-11-13 23:07:01 +0900

    fix task clean

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,6 +2,6 @@ import { clean as config } from '../config';
 import del from 'del';
 import gulp from 'gulp';
 
-gulp.task('clean', callback => {
-  del(config, callback);
+gulp.task('clean', () => {
+  return del(config);
 });
