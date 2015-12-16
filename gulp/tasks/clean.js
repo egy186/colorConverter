17b5db19c4316478f30933d4d827f6ebba488936
egy186 <egy186@yahoo.co.jp>
2015-12-17 00:48:25 +0900

    no gulp

--- gulp/tasks/clean.js
+++
@@ -1,7 +0,0 @@
-import { clean as config } from '../config';
-import del from 'del';
-import gulp from 'gulp';
-
-gulp.task('clean', () => {
-  return del(config);
-});
--- gulp/tasks/watch.js
+++
@@ -1,9 +0,0 @@
-import { watch as config } from '../config';
-import gulp from 'gulp';
-
-gulp.task('watch', ['browsersync'], () => {
-  gulp.watch(config.css, ['build:css']);
-  gulp.watch(config.html, ['build:html']);
-  gulp.watch(config.js, ['build:js']);
-  gulp.watch(config.static, ['build:static']);
-});
